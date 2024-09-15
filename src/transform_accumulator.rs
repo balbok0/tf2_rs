bfe55088db95df68717655626d335b339141789a
Jakub Filipek <jfilipek@proton.me>
2024-09-15 15:58:05 -0700

    nicer Accumulator implementation

--- src/transform_accumulator.rs
+++ src/transform_accumulator.rs
@@ -12,17 +12,24 @@ pub(crate) enum WalkEnding {
 }
 
 
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub(crate) struct TransformAccumulator {
     pub(crate) source_to_top_quat: UnitQuaternion<f64>,
     pub(crate) source_to_top_vec: Vector3<f64>,
     pub(crate) target_to_top_quat: UnitQuaternion<f64>,
     pub(crate) target_to_top_vec: Vector3<f64>,
-
-    time: u64,
-    st: TransformStorage,
 }
 
 impl TransformAccumulator {
+    pub fn new() -> TransformAccumulator {
+        TransformAccumulator {
+            source_to_top_quat: UnitQuaternion::identity(),
+            source_to_top_vec: Vector3::zeros(),
+            target_to_top_quat: UnitQuaternion::identity(),
+            target_to_top_vec: Vector3::zeros(),
+        }
+    }
+
     // pub(crate) fn gather(
     //     &self,
     //     cache: &dyn TimeCacheInterface,
@@ -33,32 +40,33 @@ impl TransformAccumulator {
 
     pub(crate) fn accum(
         &mut self,
-        source: bool
+        source: bool,
+        st: TransformStorage,
     ) {
         if source {
-            self.source_to_top_vec = self.st.rotate_vec(&self.source_to_top_vec) + self.st.translation;
-            self.source_to_top_quat = self.st.rotation * self.source_to_top_quat;
+            self.source_to_top_vec = st.rotate_vec(&self.source_to_top_vec) + st.translation;
+            self.source_to_top_quat = st.rotation * self.source_to_top_quat;
         } else {
-            self.target_to_top_vec = self.st.rotate_vec(&self.target_to_top_vec) + self.st.translation;
-            self.target_to_top_quat = self.st.rotation * self.target_to_top_quat;
+            self.target_to_top_vec = st.rotate_vec(&self.target_to_top_vec) + st.translation;
+            self.target_to_top_quat = st.rotation * self.target_to_top_quat;
         }
     }
 
     pub(crate) fn finalize(
         &self,
         end: WalkEnding,
-    ) -> Option<(Vector3<f64>, UnitQuaternion<f64>)> {
+    ) -> (Vector3<f64>, UnitQuaternion<f64>) {
         match end {
             WalkEnding::Identity => {
-                Some((Vector3::zeros(), UnitQuaternion::identity()))
+                (Vector3::zeros(), UnitQuaternion::identity())
             },
             WalkEnding::TargetParentOfSource => {
-                Some((self.source_to_top_vec, self.source_to_top_quat))
+                (self.source_to_top_vec, self.source_to_top_quat)
             },
             WalkEnding::SourceParentOfTarget => {
                 let inv_target_quat = self.target_to_top_quat.inverse();
                 let inv_target_vec = inv_target_quat.transform_vector(&-self.target_to_top_vec);
-                Some((inv_target_vec, inv_target_quat))
+                (inv_target_vec, inv_target_quat)
             },
             WalkEnding::FullPath => {
                 let inv_target_quat = self.target_to_top_quat.inverse();
@@ -67,7 +75,7 @@ impl TransformAccumulator {
                 let result_vec = inv_target_quat.transform_vector(&self.source_to_top_vec) + inv_target_vec;
                 let result_quat = inv_target_quat * self.source_to_top_quat;
 
-                Some((result_vec, result_quat))
+                (result_vec, result_quat)
             }
         }
     }
